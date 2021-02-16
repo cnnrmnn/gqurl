dc5fd9170b80465ec897b16064ecb177b514127b
cnnrmnn <connor.mann@outlook.com>
2021-02-16 11:40:06 -0500

    Changed request function to output axios metadata and stringified response data

--- src/request.js
+++ src/request.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 export default async function request(url, data, config, all) {
   try {
     const response = await axios.post(url, data, config);
-    console.log(all ? response : JSON.stringify(response.data, null, 2));
+    if (all) console.log(response);
+    console.log(JSON.stringify(response.data, null, 2));
   } catch (error) {
-    console.error(
-      all ? error.response : JSON.stringify(error.response.data, null, 2),
-    );
+    if (all) console.error(error.response);
+    console.error(JSON.stringify(error.response.data, null, 2));
   }
 }
