38c78948b89873f3a3289262c648590ce1a856a0
cnnrmnn <connor.mann@outlook.com>
2021-02-16 11:22:33 -0500

    Added encoding to file-reading function so that string is returned

--- src/file.js
+++ src/file.js
@@ -3,7 +3,7 @@ import fs from 'fs/promises';
 export default async function readFromFile(path) {
   let data;
   try {
-    data = await fs.readFile(path);
+    data = await fs.readFile(path, { encoding: 'utf-8' });
   } catch (error) {
     console.error(`Unable to load contents of file at path ${path}`);
     console.error(error);
