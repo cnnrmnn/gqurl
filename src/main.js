34c721d839905dab27d5455072d07dc48f0ea368
cnnrmnn <connor.mann@outlook.com>
2021-02-16 10:27:38 -0500

    Added command parsing with yargs

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
+import yargs from 'yargs/yargs';
+import { hideBin } from 'yargs/helpers';
+
 export default async function main() {
-  console.log(process.argv);
+  const { argv } = yargs(hideBin(process.argv))
+    .option('query', {
+      type: 'string',
+      describe: 'GraphQL query path',
+      demandOption: true,
+    })
+    .option('variables', {
+      type: 'string',
+      describe: 'GraphQL variables path (JSON)',
+    })
+    .option('headers', {
+      type: 'string',
+      describe: 'HTTP headers path',
+    })
+    .demandCommand(
+      1,
+      1,
+      'Missing required argument: <url>',
+      'Too many arguments',
+    )
+    .usage('$0 [options...] <url>')
+    .version(false)
+    .strict();
 }
